7ff4c4a31ba16108ba2e2f40f1f783959971c8d2
artemlp <artem.just.lp@gmail.com>
2016-03-08 21:12:56 +0200

    r

--- app.js
+++ app.js
@@ -45,4 +45,31 @@ fs.readFile('./main.css', 'utf8',function(err, data) {
     
     var obj = css.parse(data,{});
     //console.log(obj.stylesheet);
+
+
+    var GOBJ = {};
+
+    obj.stylesheet.rules.forEach(function (item) {
+        if (item.type === 'rule') {
+            for (var k = 0; k < item.selectors.length; k++) {
+                GOBJ[item.selectors[k]] = [];
+            }
+        }
+    });
+
+    for (var key in GOBJ) {
+        obj.stylesheet.rules.forEach(function (item) {
+            if (item.type === 'rule') {
+                var arr;
+                for (var p=0; p<item.selectors.length; p++) {
+                    if (key === item.selectors[p]) {
+                        arr = GOBJ[key].concat(item.declarations);
+                        GOBJ[key] = arr;
+                    }
+                }
+            }
+        })
+    }
+    
+    console.log(GOBJ);
 });
